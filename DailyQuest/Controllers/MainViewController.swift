5848d9ae7ec662f8aebc2e0b26f0e0b1d12bfd02
Youngking0914 <ace_lephant@naver.com>
2021-08-17 17:29:56 +0900

    TextField 에 값이 없어도 리턴됨
    if let text = inputTextField.text  이부분을 수정해야할듯
    눈으로 봤을 땐 비어있어도 ""로 인정하는 것 같음

--- DailyQuest/Controllers/MainViewController.swift
+++ DailyQuest/Controllers/MainViewController.swift
@@ -8,27 +8,17 @@
 import UIKit
 
 // 투두리스트를 담을 전역변수
-// var list = [TableViewCell]()
+var list = [TodoList]()
 
-class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class MainViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate {
     
     @IBOutlet weak var questTableView: UITableView!
-    @IBOutlet weak var currentTimeLabel: UILabel?
-    
-    func showDate() {
-        let date = Date() // YYYY-MM-dd EEE 2021-08-05 목요일
-        let dateFormatter = DateFormatter() // 날짜 표시형식 변경
-        dateFormatter.locale = Locale(identifier: "ko-KR")
-        dateFormatter.dateFormat = "MM월 dd일 E"
-        
-        currentTimeLabel?.text = dateFormatter.string(from: date)
-    }
+    @IBOutlet weak var inputTextField: UITextField!
     
     override func viewDidLoad() {
         questTableView.dataSource = self
         questTableView.delegate = self
-        
-        showDate()
+        inputTextField.delegate = self
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
@@ -44,9 +34,34 @@ class MainViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return cell
     }
     
-    @IBAction func action(_ sender: Any) {
-        
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // return 키를 입력한 textField 가 inputTextField 일 경우
+        if textField == inputTextField {
+            // textField 안에 내용이 있을 경우에만
+            if let text = inputTextField.text {
+                print(text + " return") // 테스트용
+                list.append(TodoList(content: String(text), isComplete: false))
+            }
+            // 테이블 뷰 셀에 TodoList 값 넣기
+            //
+            // 테이블 뷰 reload 새로고침
+            //
+        }
+        return true
     }
+    
+    /*
+    func showDate() {
+        let date = Date() // YYYY-MM-dd EEE 2021-08-05 목요일
+        let dateFormatter = DateFormatter() // 날짜 표시형식 변경
+        dateFormatter.locale = Locale(identifier: "ko-KR")
+        dateFormatter.dateFormat = "MM월 dd일 E"
+        
+        currentTimeLabel?.text = dateFormatter.string(from: date)
+    }
+ */
+    
+    
     
 }
 
--- DailyQuest/Models/TodoList.swift
+++ DailyQuest/Models/TodoList.swift
@@ -8,5 +8,11 @@
 import UIKit
 
 struct TodoList {
+    var content: String? = "" // 할 일 내용
+    var isComplete: Bool = false // 할 일 완료 여부
     
+    init(content: String?, isComplete: Bool ) {
+        self.content = content
+        self.isComplete = isComplete
+    }
 }
